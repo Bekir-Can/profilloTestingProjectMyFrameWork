21a6c829ca96e028c089bb26253483cbeb8ca743
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-03-05 01:09:56 +0100

    last update for Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main");
+        System.out.println("This is a class inside main folder therefore used by developer");
     }
 }
