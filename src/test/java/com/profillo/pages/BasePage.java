3f0f5c157cc39572341cc046da848e5b7a7e5690
5-mahri <5-mahri@users.noreply.github.com>
2021-03-06 01:05:07 +0100

    delete unnecessary lines on BasePage.java

--- src/test/java/com/profillo/pages/BasePage.java
+++ src/test/java/com/profillo/pages/BasePage.java
@@ -32,40 +32,5 @@ public abstract class BasePage {
 
 
 
-    /**
-     * Waits until loader screen present. If loader screen will not pop up at all,
-     * NoSuchElementException will be handled  bu try/catch block
-     * Thus, we can continue in any case.
-     */
-//    public void waitUntilLoaderScreenDisappear() {
-//        try {
-//            WebDriverWait wait = new WebDriverWait(Driver.get(), 7);
-//            wait.until( ExpectedConditions.invisibilityOf(loaderMask));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
-//    public String getUserName(){
-//        waitUntilLoaderScreenDisappear();
-//        BrowserUtils.waitForVisibility(userName, 7);
-//        return userName.getText();
-//    }
-//
-//
-//
-//    public void logOut(){
-//        BrowserUtils.waitFor(2);
-//        BrowserUtils.clickWithJS(userName);
-//        BrowserUtils.clickWithJS(logOutLink);
-//    }
-//    public void goToMyUser(){
-//        waitUntilLoaderScreenDisappear();
-//        BrowserUtils.waitForClickablility(userName, 5).click();
-//        BrowserUtils.waitForClickablility(myUser, 5).click();
-//
-//    }
-
 
 }
