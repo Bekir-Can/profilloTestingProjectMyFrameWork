8a2fb11974c7c9e7393f747d3edab51a63b26a63
5-mahri <5-mahri@users.noreply.github.com>
2021-03-09 10:47:50 +0100

    6 ac.cr of Edit User is ready

--- src/test/java/com/profillo/pages/UserManagementPage.java
+++ src/test/java/com/profillo/pages/UserManagementPage.java
@@ -1,10 +1,13 @@
 package com.profillo.pages;
 
+import com.profillo.utilities.BrowserUtils;
 import com.profillo.utilities.Driver;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
 import java.util.Random;
 
 public class UserManagementPage extends BasePage{
@@ -20,6 +23,38 @@ public class UserManagementPage extends BasePage{
 
     @FindBy(xpath = "//button[text()='Close']")
     public WebElement closeWindow;
+
+    @FindBy(id = "user_groups")
+    public WebElement userGroupBox;
+
+    @FindBy(xpath="//table//tbody//tr/td[5]")////table//tbody//tr[2]/td[5]
+    public List<WebElement> table;
+
+    public void studentsColumn(){
+        BrowserUtils.waitFor( 2 );
+
+        String category = null;
+
+        for (int i = 1; i <= table.size(); i++) {
+            String xpath = "//table//tbody//tr[" + i + "]/td[5]";
+            category = Driver.get().findElement( By.xpath( xpath ) ).getText();
+            Assert.assertEquals( "Students",category );
+        }
+    }
+
+    public void librarianColumn(){
+        BrowserUtils.waitFor( 2 );
+
+        String category = null;
+
+        for (int i = 1; i <= table.size(); i++) {
+            String xpath = "//table//tbody//tr[" + i + "]/td[5]";
+            category = Driver.get().findElement( By.xpath( xpath ) ).getText();
+            Assert.assertEquals( "Librarian",category );
+        }
+    }
+
+
 
     public void chooseRandomEditUser(){
         Random rn = new Random();
