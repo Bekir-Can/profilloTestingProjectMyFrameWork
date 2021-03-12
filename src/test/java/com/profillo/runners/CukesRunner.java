4b69d09db0ac9ccdf4393c34da7be88bacb42036
5-mahri <71532978+5-mahri@users.noreply.github.com>
2021-03-12 10:43:36 +0100

    Update CukesRunner.java

--- src/test/java/com/profillo/runners/CukesRunner.java
+++ src/test/java/com/profillo/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/",
         glue = "com/profillo/step_definitions",
         dryRun = false,
-        tags = "@EditBook2"
+        tags = "@PROF-330"
 )
 public class CukesRunner {
 }
