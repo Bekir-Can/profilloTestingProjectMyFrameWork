f45745a8e2c11e1084256602704d217d3210fc1b
Bekir-Can <ruikosta03@gmail.com>
2021-03-12 17:13:38 +0200

    last update for Barrow Book Function

--- src/test/java/com/profillo/pages/BarrowBookManagementPage.java
+++ src/test/java/com/profillo/pages/BarrowBookManagementPage.java
@@ -1,9 +1,8 @@
 package com.profillo.pages;
 
-
-
 import com.profillo.utilities.BrowserUtils;
 import com.profillo.utilities.Driver;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -23,25 +22,25 @@ public class BarrowBookManagementPage extends BasePage {
     @FindBy(xpath = "//*[@id=\"tbl_books_paginate\"]/ul/li[8]/a/i")
     public WebElement changePage;
 
-    public void selectEntitiesNumber(String number){
+    public void selectEntitiesNumber(String number) {
         Select select = new Select(entityDropDown);
         select.selectByValue(number);
     }
 
-    public void borrowBookAction(){
+    public void borrowBookAction() {
 
         Select select = new Select(entityDropDown);
         Integer number = Integer.parseInt(select.getFirstSelectedOption().getText());
-        for (int i = 1; i<=number;i++){
+        for (int i = 1; i <= number; i++) {
             try {
-                BrowserUtils.waitForClickablility(Driver.get().findElement(By.xpath("//tbody/tr[" + i + "]/td[1]/a")),2);
+                BrowserUtils.waitForClickablility(Driver.get().findElement(By.xpath("//tbody/tr[" + i + "]/td[1]/a")), 2);
                 WebElement element = Driver.get().findElement(By.xpath("//tbody/tr[" + i + "]/td[1]/a"));
                 element.click();
                 System.out.println("this try block run");
                 break;
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("this borrow button is not clickable");
-                if (i% number == 0){
+                if (i % number == 0) {
                     i = 1;
                     changePage.click();
                 }
@@ -50,5 +49,96 @@ public class BarrowBookManagementPage extends BasePage {
 
     }
 
-}
+    @FindBy(xpath = "//tbody/tr[1]/td[1]")
+    public WebElement inactiveButton;
 
+    @FindBy(xpath = "//*[@id=\"menu_item\"]/li[2]/a/span[1]")
+    public WebElement BorrowingBookModule;
+
+    @FindBy(xpath = "//*[@id=\"borrowed_list\"]/tbody/tr[1]/td[1]")
+    public WebElement returnBookButton;
+
+    @FindBy(xpath = "//*[@id=\"borrowing-books\"]/div[1]/div[1]/h3")
+    public WebElement borrowingBookTitle;
+
+
+    public void borrowingBookHistory() {
+
+        for (int i = 170; i <= 190; i++) {
+            try {
+                BrowserUtils.waitForClickablility(Driver.get().findElement(By.xpath("//tbody/tr[" + i + "]/td[1]/a")), 2);
+                WebElement element = Driver.get().findElement(By.xpath("//tbody/tr[" + i + "]/td[1]/a"));
+                element.click();
+                System.out.println("this try block run");
+                break;
+            } catch (Exception e) {
+                System.out.println("this borrow button is not clickable");
+
+            }
+        }
+
+    }
+
+    @FindBy(id = "book_categories")
+    public WebElement categoryDropdown;
+
+    @FindBy(xpath = "//table//tbody//tr/td[5]")
+    public List<WebElement> table;
+
+    public void selectCategory() {
+
+        BrowserUtils.waitFor(2);
+        String category = null;
+        for (int i = 1; i <= table.size(); i++) {
+            String xpath = "//table[@id='tbl_books']/tbody[1]/tr[" + i + "]/td[5]";
+            category = Driver.get().findElement(By.xpath(xpath)).getText();
+            Assert.assertEquals("Classic", category);
+        }
+    }
+
+    public void bookCategory(List<String> bookOptions) {
+        Select select = new Select(new BarrowBookManagementPage().categoryDropdown);
+        List<WebElement> options = select.getOptions();
+        for (WebElement option : options) {
+            System.out.println(option.getText());
+        }
+        List<String> actualOptions = BrowserUtils.getElementsText(select.getOptions());
+        Assert.assertEquals(bookOptions, actualOptions);
+    }
+
+    @FindBy(name = "tbl_books_length")
+    public WebElement recordBox;
+
+    @FindBy(xpath = "//tbody/tr")
+    public List<WebElement> sizeTabe;
+
+    @FindBy(tagName = "input")
+    public WebElement searchBox;
+
+
+    public void BookName() {
+        List<WebElement> nameBook = new BarrowBookManagementPage().sizeTabe;
+        BrowserUtils.waitFor(3);
+
+        for (int i = 1; i <= nameBook.size(); i++) {
+            String bookName = Driver.get().findElement(By.xpath("//table/tbody/tr[" + i + "]/td[3]")).getText();
+            BrowserUtils.waitFor(2);
+            Assert.assertEquals("The Farm of AnimalPersuation", bookName);
+
+        }
+    }
+
+    public void Author() {
+        List<WebElement> nameBook = new BarrowBookManagementPage().sizeTabe;
+        BrowserUtils.waitFor(3);
+
+        for (int i = 1; i <= nameBook.size(); i++) {
+            String authorName = Driver.get().findElement(By.xpath("//table/tbody/tr[" + i + "]/td[4]")).getText();
+            BrowserUtils.waitFor(2);
+            Assert.assertEquals("Cervantes", authorName);
+
+        }
+
+    }
+
+}
